3b2ecb281cf0d20c45235ed70b809733244852ab
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2022-11-03 12:07:31 +0100

    Fix: Fixed remote git repository

--- src/Components/ContactBody.jsx
+++ src/Components/ContactBody.jsx
@@ -4,8 +4,8 @@ const ContactBody = () => {
   return (
     <div>
         <div>
-            <h3 className='text-primary'>Contact Me</h3>
-            <p className='text-secondary'>Hi there, contact me to ask me about anything you have in mind</p>
+            <h3 className='text-primary mt-[7.75rem] mx-4 text-4xl font-semibold leading-10'>Contact Me</h3>
+            <p className='text-secondary m-4'>Hi there, contact me to ask me about anything you have in mind</p>
         </div>
     </div>
   )
